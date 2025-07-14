d7681680b75abd83ba5b889ae9b283d529cb3fa9
solanomillo <solanomillo144@gmail.con>
2025-07-14 16:50:04 -0300

    Agrega .gitattributes para normalizar saltos de línea

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Categoria, Producto
 # Register your models here.
+
+@admin.register(Categoria)
+class AdminCategoria(admin.ModelAdmin):
+    list_display = ('nombre','fecha_registro')
+    readonly_fields = ('fecha_registro',)
+
+
+@admin.register(Producto)
+class AdminProducto(admin.ModelAdmin):
+    list_display = ('categoria', 'nombre', 'precio','imagen')
+    list_editable = ('precio',)
+    list_filter = ('categoria__nombre',)
+    search_fields = ('categoria__nombre', 'nombre')
+    readonly_fields = ('fecha_registro',)
+
