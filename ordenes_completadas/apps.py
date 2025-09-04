ab4a2aa703a2e1610e12d75661d0f9204320ebdc
solanomillo <solanomillo144@gmail.con>
2025-09-04 15:05:55 -0300

    agregue la app ordenes_completadas

---
+++ ordenes_completadas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OrdenesCompletadasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ordenes_completadas'
--- tiendaonile/settings/dev.py
+++ tiendaonile/settings/dev.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'paypal.standard.ipn',
     'jazzmin',
     'Codigo_promocion',
+    'ordenes_completadas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
